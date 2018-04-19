549fdabfd78311f02987f50e7b465ee3301582f5
hecomi <info@hecomi.com>
2018-04-20 08:26:13 +0900

    fix indent.

--- Assets/uDllExporter/Editor/DllExporter.cs
+++ Assets/uDllExporter/Editor/DllExporter.cs
@@ -33,366 +33,366 @@ namespace uDllExporter
 
 public class DllExporter : ScriptableWizard
 {
-	const string BuildErrorMessage = "Build Error (see Consele output).";
-	const string UnknownErrorMessage =  "Export Error (Please report the error message to uDllExporter developer).";
+    const string BuildErrorMessage = "Build Error (see Consele output).";
+    const string UnknownErrorMessage =  "Export Error (Please report the error message to uDllExporter developer).";
 
-	public enum TargetDirectory
-	{
-		All,
-		UnityEngine,
-		UnityEditor,
-	}
+    public enum TargetDirectory
+    {
+        All,
+        UnityEngine,
+        UnityEditor,
+    }
 
-	[SerializeField]
-	TargetDirectory targetDirectory = TargetDirectory.All;
+    [SerializeField]
+    TargetDirectory targetDirectory = TargetDirectory.All;
 
-	[SerializeField]
-	string outputDirectoryPath;
+    [SerializeField]
+    string outputDirectoryPath;
 
-	[SerializeField]
-	string outputDllName;
+    [SerializeField]
+    string outputDllName;
 
-	struct DllInfo
-	{
-		public string name;
-		public string path;
-	}
-	List<DllInfo> unityEngineDllList;
-	List<DllInfo> unityEditorDllList;
-	List<DllInfo> otherDllList;
+    struct DllInfo
+    {
+        public string name;
+        public string path;
+    }
+    List<DllInfo> unityEngineDllList;
+    List<DllInfo> unityEditorDllList;
+    List<DllInfo> otherDllList;
 
-	int unityEngineDllListFlags = 1;
-	int unityEditorDllListFlags = 1;
-	int otherDllListFlags = 0;
+    int unityEngineDllListFlags = 1;
+    int unityEditorDllListFlags = 1;
+    int otherDllListFlags = 0;
 
-	List<string> files = null;
-	Vector2 filesScrollPos = Vector2.zero;
-	string outputInfo;
+    List<string> files = null;
+    Vector2 filesScrollPos = Vector2.zero;
+    string outputInfo;
 
-	static string unityEditorPath
-	{
-		get
-		{
-			var appPath = System.Environment.GetCommandLineArgs()[0];
-			var appDir = Path.GetDirectoryName(appPath);
+    static string unityEditorPath
+    {
+        get
+        {
+            var appPath = System.Environment.GetCommandLineArgs()[0];
+            var appDir = Path.GetDirectoryName(appPath);
 #if UNITY_EDITOR_WIN
-			return appDir + "/Data/";
+            return appDir + "/Data/";
 #else
-			return appDir + "/../";
+            return appDir + "/../";
 #endif
-		}
-	}
+        }
+    }
 
-	static string unityEngineDllPath
-	{
-		get	{ return unityEditorPath + "Managed/UnityEngine.dll"; }
-	}
+    static string unityEngineDllPath
+    {
+        get    { return unityEditorPath + "Managed/UnityEngine.dll"; }
+    }
 
-	static string unityEditorDllPath
-	{
-		get	{ return unityEditorPath + "Managed/UnityEditor.dll"; }
-	}
+    static string unityEditorDllPath
+    {
+        get    { return unityEditorPath + "Managed/UnityEditor.dll"; }
+    }
 
-	static string unityExtensionsDirPath
-	{
-		get	{ return unityEditorPath + "UnityExtensions"; }
-	}
+    static string unityExtensionsDirPath
+    {
+        get    { return unityEditorPath + "UnityExtensions"; }
+    }
 
-	static string smcsPath
-	{
+    static string smcsPath
+    {
 #if UNITY_EDITOR_WIN
-		get	{ return unityEditorPath + "Mono/bin/smcs.bat"; }
+        get    { return unityEditorPath + "Mono/bin/smcs.bat"; }
 #else
-		get	{ return unityEditorPath + "Mono/bin/smcs"; }
+        get    { return unityEditorPath + "Mono/bin/smcs"; }
 #endif
-	}
+    }
 
     [MenuItem("Window/uDllExporter")]
     static void Open()
     {
         var window = DisplayWizard<DllExporter>("uDllExporter", "Close", "Build DLL");        
-		window.InitOutputParameters();
-		window.InitDllList();
-	}
+        window.InitOutputParameters();
+        window.InitDllList();
+    }
 
-	void InitOutputParameters()
-	{
-		var projectDir = Directory.GetParent(Application.dataPath);
-		outputDirectoryPath = projectDir.FullName + "/Build";
-		outputDllName = projectDir.Name + ".dll";
-	}
+    void InitOutputParameters()
+    {
+        var projectDir = Directory.GetParent(Application.dataPath);
+        outputDirectoryPath = projectDir.FullName + "/Build";
+        outputDllName = projectDir.Name + ".dll";
+    }
 
-	void InitDllList()
-	{
-		unityEngineDllList = new List<DllInfo>();
-		unityEditorDllList = new List<DllInfo>();
-		otherDllList = new List<DllInfo>();
+    void InitDllList()
+    {
+        unityEngineDllList = new List<DllInfo>();
+        unityEditorDllList = new List<DllInfo>();
+        otherDllList = new List<DllInfo>();
 
-		unityEngineDllList.Add(new DllInfo { name = "UnityEngine.dll", path = unityEngineDllPath });
-		unityEditorDllList.Add(new DllInfo { name = "UnityEditor.dll", path = unityEditorDllPath });
+        unityEngineDllList.Add(new DllInfo { name = "UnityEngine.dll", path = unityEngineDllPath });
+        unityEditorDllList.Add(new DllInfo { name = "UnityEditor.dll", path = unityEditorDllPath });
 
-		foreach (var path in GetUnityExtensionDllPaths())
-		{
-			var name = Path.GetFileName(path);
-			var info = new DllInfo { name = Path.GetFileName(path), path = path };
-			if (name.IndexOf("UnityEngine") != -1)
-			{
-				unityEngineDllList.Add(info);
-			}
-			else if (name.IndexOf("UnityEditor") != -1)
-			{
-				unityEditorDllList.Add(info);
-			}
-			else
-			{
-				otherDllList.Add(info);
-			}
-		}
-	}
+        foreach (var path in GetUnityExtensionDllPaths())
+        {
+            var name = Path.GetFileName(path);
+            var info = new DllInfo { name = Path.GetFileName(path), path = path };
+            if (name.IndexOf("UnityEngine") != -1)
+            {
+                unityEngineDllList.Add(info);
+            }
+            else if (name.IndexOf("UnityEditor") != -1)
+            {
+                unityEditorDllList.Add(info);
+            }
+            else
+            {
+                otherDllList.Add(info);
+            }
+        }
+    }
 
-	protected override bool DrawWizardGUI()
-	{
-		EditorGUILayout.LabelField("Export Settings", EditorStyles.boldLabel);
+    protected override bool DrawWizardGUI()
+    {
+        EditorGUILayout.LabelField("Export Settings", EditorStyles.boldLabel);
 
-		++EditorGUI.indentLevel;
-		{
-			base.DrawWizardGUI();
+        ++EditorGUI.indentLevel;
+        {
+            base.DrawWizardGUI();
 
-			if (unityEditorDllList == null || unityEditorDllList.Count == 0 ||
-				unityEditorDllList == null || unityEngineDllList.Count == 0 ||
-				otherDllList == null || otherDllList.Count == 0)
-			{
-				InitDllList();
-			}
+            if (unityEditorDllList == null || unityEditorDllList.Count == 0 ||
+                unityEditorDllList == null || unityEngineDllList.Count == 0 ||
+                otherDllList == null || otherDllList.Count == 0)
+            {
+                InitDllList();
+            }
 
-			unityEngineDllListFlags = EditorGUILayout.MaskField(
-				"Unity Engine DLLs", 
-				unityEngineDllListFlags, 
-				unityEngineDllList.Select(x => x.name).ToArray());
+            unityEngineDllListFlags = EditorGUILayout.MaskField(
+                "Unity Engine DLLs", 
+                unityEngineDllListFlags, 
+                unityEngineDllList.Select(x => x.name).ToArray());
 
-			unityEditorDllListFlags = EditorGUILayout.MaskField(
-				"Unity Editor DLLs", 
-				unityEditorDllListFlags, 
-				unityEditorDllList.Select(x => x.name).ToArray());
+            unityEditorDllListFlags = EditorGUILayout.MaskField(
+                "Unity Editor DLLs", 
+                unityEditorDllListFlags, 
+                unityEditorDllList.Select(x => x.name).ToArray());
 
-			otherDllListFlags = EditorGUILayout.MaskField(
-				"Other DLLs", 
-				otherDllListFlags, 
-				otherDllList.Select(x => x.name).ToArray());
-		}
-		--EditorGUI.indentLevel;
+            otherDllListFlags = EditorGUILayout.MaskField(
+                "Other DLLs", 
+                otherDllListFlags, 
+                otherDllList.Select(x => x.name).ToArray());
+        }
+        --EditorGUI.indentLevel;
 
-		EditorGUILayout.Space();
+        EditorGUILayout.Space();
 
-		if (files != null)
-		{
-			EditorGUILayout.LabelField("Exported Files", EditorStyles.boldLabel);
-			++EditorGUI.indentLevel;
-			{
-				filesScrollPos = EditorGUILayout.BeginScrollView(filesScrollPos);
-				var assetsPathLength = Application.dataPath.Length + 1;
-				foreach (var file in files)
-				{
-					EditorGUILayout.LabelField(file.Substring(assetsPathLength));
-				}
-				EditorGUILayout.EndScrollView();
-			}
-			--EditorGUI.indentLevel;
-		}
+        if (files != null)
+        {
+            EditorGUILayout.LabelField("Exported Files", EditorStyles.boldLabel);
+            ++EditorGUI.indentLevel;
+            {
+                filesScrollPos = EditorGUILayout.BeginScrollView(filesScrollPos);
+                var assetsPathLength = Application.dataPath.Length + 1;
+                foreach (var file in files)
+                {
+                    EditorGUILayout.LabelField(file.Substring(assetsPathLength));
+                }
+                EditorGUILayout.EndScrollView();
+            }
+            --EditorGUI.indentLevel;
+        }
 
-		if (!string.IsNullOrEmpty(outputInfo))
-		{
-			EditorGUILayout.LabelField("Output", EditorStyles.boldLabel);
-			++EditorGUI.indentLevel;
-			EditorGUILayout.LabelField(outputInfo);
-			--EditorGUI.indentLevel;
-		}
-		
-		EditorGUILayout.Space();
+        if (!string.IsNullOrEmpty(outputInfo))
+        {
+            EditorGUILayout.LabelField("Output", EditorStyles.boldLabel);
+            ++EditorGUI.indentLevel;
+            EditorGUILayout.LabelField(outputInfo);
+            --EditorGUI.indentLevel;
+        }
+        
+        EditorGUILayout.Space();
 
-		return true;
-	}
+        return true;
+    }
 
     void OnWizardCreate()
     {
     }
 
-	void OnWizardOtherButton()
-	{
-		CreateOutputDirectory();
-		ExportDll();
-	}
-	
-	void CreateOutputDirectory()
-	{
+    void OnWizardOtherButton()
+    {
+        CreateOutputDirectory();
+        ExportDll();
+    }
+    
+    void CreateOutputDirectory()
+    {
         if (!Directory.Exists(outputDirectoryPath))
         {
             Directory.CreateDirectory(outputDirectoryPath);
         }
-	}
+    }
 
-	void ExportDll()
-	{
-		errorString = "";
+    void ExportDll()
+    {
+        errorString = "";
 
-		try
-		{
-			files = GetSelectedFilePaths();
-			var dlls = GetSelectedDllPaths();
+        try
+        {
+            files = GetSelectedFilePaths();
+            var dlls = GetSelectedDllPaths();
 
-			var process = new System.Diagnostics.Process();
-			process.StartInfo.FileName = smcsPath;
-			process.StartInfo.Arguments = GetArguments(files, dlls);
-			process.StartInfo.RedirectStandardOutput = true;
+            var process = new System.Diagnostics.Process();
+            process.StartInfo.FileName = smcsPath;
+            process.StartInfo.Arguments = GetArguments(files, dlls);
+            process.StartInfo.RedirectStandardOutput = true;
             process.StartInfo.RedirectStandardError = true;         
-			process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.CreateNoWindow = true;
             process.StartInfo.UseShellExecute = false; 
-			process.Start();
+            process.Start();
 
-			string stdout = process.StandardOutput.ReadToEnd();         
+            string stdout = process.StandardOutput.ReadToEnd();         
             string stderr = process.StandardError.ReadToEnd();             
-			process.WaitForExit();
+            process.WaitForExit();
 
-			if (!string.IsNullOrEmpty(stdout))
-			{
-				stdout = stdout.Trim();
-				Debug.LogWarning(stdout);
-			}
+            if (!string.IsNullOrEmpty(stdout))
+            {
+                stdout = stdout.Trim();
+                Debug.LogWarning(stdout);
+            }
 
-			if (!string.IsNullOrEmpty(stderr))
-			{
-				stderr = stderr.Trim();
-				if (stderr.IndexOf(": error") != -1 || stderr.IndexOf("Exception:") != -1)
-				{
-					Debug.LogError(stderr);
-					errorString = BuildErrorMessage;
-				}
-				else
-				{
-					Debug.LogWarning(stderr);
-				}
-			}
-		}
-		catch (System.Exception e)
-		{
-			Debug.LogError(e.Message);
-			Debug.LogError(e.StackTrace);
-			errorString = UnknownErrorMessage;
-		}
+            if (!string.IsNullOrEmpty(stderr))
+            {
+                stderr = stderr.Trim();
+                if (stderr.IndexOf(": error") != -1 || stderr.IndexOf("Exception:") != -1)
+                {
+                    Debug.LogError(stderr);
+                    errorString = BuildErrorMessage;
+                }
+                else
+                {
+                    Debug.LogWarning(stderr);
+                }
+            }
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError(e.Message);
+            Debug.LogError(e.StackTrace);
+            errorString = UnknownErrorMessage;
+        }
 
-		if (string.IsNullOrEmpty(errorString))
-		{
-			var path = string.Format("{0}/{1}", outputDirectoryPath, outputDllName);
-			if (!File.Exists(path))
-			{
-				errorString = UnknownErrorMessage;
-			}
-			else
-			{
-				var outputDllSize = new FileInfo(path).Length;
-				outputInfo = string.Format("{0}/{1} ({2} byte)", 
-					outputDirectoryPath, 
-					outputDllName,
-					outputDllSize);
-			}
-		}
-	}
+        if (string.IsNullOrEmpty(errorString))
+        {
+            var path = string.Format("{0}/{1}", outputDirectoryPath, outputDllName);
+            if (!File.Exists(path))
+            {
+                errorString = UnknownErrorMessage;
+            }
+            else
+            {
+                var outputDllSize = new FileInfo(path).Length;
+                outputInfo = string.Format("{0}/{1} ({2} byte)", 
+                    outputDirectoryPath, 
+                    outputDllName,
+                    outputDllSize);
+            }
+        }
+    }
 
-	void GetFilesRecursively(string path, ref List<string> list)
-	{
-		if (Directory.Exists(path))
-		{
-			foreach (var dir in Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories))
-			{
-				GetFilesRecursively(Path.GetFullPath(dir), ref list);
-			}
-		}
-		else if (File.Exists(path))
-		{
-			if (path.EndsWith(".cs")) list.Add(Path.GetFullPath(path));
-		}
-	}
+    void GetFilesRecursively(string path, ref List<string> list)
+    {
+        if (Directory.Exists(path))
+        {
+            foreach (var dir in Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories))
+            {
+                GetFilesRecursively(Path.GetFullPath(dir), ref list);
+            }
+        }
+        else if (File.Exists(path))
+        {
+            if (path.EndsWith(".cs")) list.Add(Path.GetFullPath(path));
+        }
+    }
 
-	bool IsEditorScript(string path)
-	{
-		return 
+    bool IsEditorScript(string path)
+    {
+        return 
             path.IndexOf("/Editor/") != -1 ||
             path.IndexOf("\\Editor\\") != -1;
-	}
-	
-	List<string> GetSelectedFilePaths()
-	{
-		var list = new List<string>();
+    }
+    
+    List<string> GetSelectedFilePaths()
+    {
+        var list = new List<string>();
 
         foreach (var obj in Selection.objects)
         {   
-			var path = AssetDatabase.GetAssetPath(obj);
-			GetFilesRecursively(path, ref list);
+            var path = AssetDatabase.GetAssetPath(obj);
+            GetFilesRecursively(path, ref list);
         }
 
-		switch (targetDirectory)
-		{
-			case TargetDirectory.All:
-				break;
-			case TargetDirectory.UnityEngine:
-				list = list.Where(x => !IsEditorScript(x)).ToList();
-				break;
-			case TargetDirectory.UnityEditor:
-				list = list.Where(x => IsEditorScript(x)).ToList();
-				break;
-		}
+        switch (targetDirectory)
+        {
+            case TargetDirectory.All:
+                break;
+            case TargetDirectory.UnityEngine:
+                list = list.Where(x => !IsEditorScript(x)).ToList();
+                break;
+            case TargetDirectory.UnityEditor:
+                list = list.Where(x => IsEditorScript(x)).ToList();
+                break;
+        }
 
-		return list;
-	}
+        return list;
+    }
 
-	List<string> GetUnityExtensionDllPaths()
-	{
-		var list = new List<string>();
+    List<string> GetUnityExtensionDllPaths()
+    {
+        var list = new List<string>();
 
-		foreach (var path in Directory.GetFiles(unityExtensionsDirPath, "*.dll", SearchOption.AllDirectories))
-		{
-			list.Add(path);
-		}
+        foreach (var path in Directory.GetFiles(unityExtensionsDirPath, "*.dll", SearchOption.AllDirectories))
+        {
+            list.Add(path);
+        }
 
-		return list;
-	}
+        return list;
+    }
 
-	List<string> GetSelectedDllPath(List<DllInfo> list, int flags)
-	{
-		var selectedList = new List<string>();
+    List<string> GetSelectedDllPath(List<DllInfo> list, int flags)
+    {
+        var selectedList = new List<string>();
 
-		for (int i = 0; i < list.Count; ++i)
-		{
-			if ((flags & 1 << i) != 0)
-			{
-				selectedList.Add(list[i].path);
-			}
-		}
+        for (int i = 0; i < list.Count; ++i)
+        {
+            if ((flags & 1 << i) != 0)
+            {
+                selectedList.Add(list[i].path);
+            }
+        }
 
-		return selectedList;
-	}
+        return selectedList;
+    }
 
-	List<string> GetSelectedDllPaths()
-	{
-		var list = new List<string>();
-		list.AddRange(GetSelectedDllPath(unityEngineDllList, unityEngineDllListFlags));
-		list.AddRange(GetSelectedDllPath(unityEditorDllList, unityEditorDllListFlags));
-		list.AddRange(GetSelectedDllPath(otherDllList, otherDllListFlags));
-		return list;
-	}
+    List<string> GetSelectedDllPaths()
+    {
+        var list = new List<string>();
+        list.AddRange(GetSelectedDllPath(unityEngineDllList, unityEngineDllListFlags));
+        list.AddRange(GetSelectedDllPath(unityEditorDllList, unityEditorDllListFlags));
+        list.AddRange(GetSelectedDllPath(otherDllList, otherDllListFlags));
+        return list;
+    }
 
-	string GetArguments(List<string> files, List<string> dlls)
-	{
-		var arguments = "";
-		arguments += string.Join(" ", dlls.Select(x => string.Format("-r:\"{0}\" ", x)).ToArray());
-		arguments += "-target:library ";
-		arguments += string.Format("-out:\"{0}/{1}\"", outputDirectoryPath, outputDllName);
-		arguments += " " + string.Join(" ", files.Select(x => string.Format("\"{0}\"", x)).ToArray());
+    string GetArguments(List<string> files, List<string> dlls)
+    {
+        var arguments = "";
+        arguments += string.Join(" ", dlls.Select(x => string.Format("-r:\"{0}\" ", x)).ToArray());
+        arguments += "-target:library ";
+        arguments += string.Format("-out:\"{0}/{1}\"", outputDirectoryPath, outputDllName);
+        arguments += " " + string.Join(" ", files.Select(x => string.Format("\"{0}\"", x)).ToArray());
 
-		Debug.Log(arguments);
+        Debug.Log(arguments);
 
-		return arguments;
-	}
+        return arguments;
+    }
 }
 
 }
